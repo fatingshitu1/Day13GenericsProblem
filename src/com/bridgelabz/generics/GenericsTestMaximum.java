b1250d250de7df716c11494d1b5818e33824cd86
shitalfating <shitalfating84@gmail.com>
2023-02-01 19:48:31 +0530

    Added:Welcome to the Generics problem

---
+++ src/com/bridgelabz/generics/GenericsTestMaximum.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.generics;
+
+public class GenericsTestMaximum {
+    public static void findMaximumValue (Integer a, Integer b, Integer c){
+        Integer max = a;
+        if (Integer b > 0){
+        System.out.println();
+
+    }
+}
