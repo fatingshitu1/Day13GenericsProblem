3d3268775c90e1e60be5ec9f4a1b7564611bfe11
shitalfating <shitalfating84@gmail.com>
2023-02-01 23:20:46 +0530

    Added: Problem by using Generics

--- src/com/bridgelabz/generics/GenericsTestMaximum.java
+++ src/com/bridgelabz/generics/GenericsTestMaximum.java
@@ -1,10 +1,18 @@
 package com.bridgelabz.generics;
 
 public class GenericsTestMaximum {
-    public static void findMaximumValue (Integer a, Integer b, Integer c){
+    public static void findMaxValue(Integer a, Integer b, Integer c) {
         Integer max = a;
-        if (Integer b > 0){
-        System.out.println();
+        if (b.compareTo(max) > 0)
+            max = b;
+        if (c.compareTo(max) > 0)
+            max = c;
+        // Print the max number
+        System.out.println("Maximum number out of 3 numbers is :- " + max);
+    }
+
+    public static void main(String[] args) {
+        GenericsTestMaximum.findMaxValue(100, 50, 75);
 
     }
 }
